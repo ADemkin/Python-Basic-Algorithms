f7179c869ac5f5c1483f255fc583660946afbe7e
ADemkin <antondemkin@yandex.ru>
2017-03-06 05:43:30 +0300

    First version of Linear Search and Binary Search algorythms. More to come.

---
+++ Bubble Sort.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+#
+# My realisation of bubble sort algorithm
+#
+# Anton Demkin, 2017
+#
+
+def bubbleSort(inputList):
+    '''This is one of simplest sorting algorithms, also one of the slowest.
+    It requires a list on input and returns a new list.
+    Usage: bubbleSort(list)'''
